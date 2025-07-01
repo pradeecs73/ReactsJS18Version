f7dd99bd05612f79d6399a9940e13358b38510aa
pradeep <pradeecs73@gmail.com>
2025-07-01 23:59:03 +0530

    feat:rubycommit

---
+++ src/components/Rubycubeexamplereact/Rubycubeexamplereact.tsx
@@ -0,0 +1,55 @@
+import React,{useState,useEffect} from 'react'
+
+const Rubycubyexamplereact= (props:any) => {
+    const myarryCopy=[true,true,true,true,false,false,true,true,true]
+    const [myarry,setMyarray]=useState(myarryCopy)
+    
+    useEffect(()=>{
+
+    })
+
+
+    const changeColor=(index:any,array:any,event:any)=>{
+        let newArray=[...array]
+        if( newArray[index]){
+            newArray[index]=false
+           setMyarray(newArray)
+          const booleanValue=newArray.some((item)=>item)
+           if(!booleanValue){
+              setMyarray(myarryCopy)
+             }else{
+                setTimeout(()=>{
+                  event.target.style.backgroundColor ="green"
+               },100)
+             }
+           
+        }
+       
+    }
+
+    const renderCube=(array:any)=>{
+ 
+           return( array.map((item:any,index:any)=>{
+                   return (
+                       <div  onClick={(event)=>changeColor(index,array,event)}
+                           style={{backgroundColor:item?'red':undefined,width:'20px',border:'1px solid black',height:'20px'}} key={index}>
+                           
+                       </div>
+                     
+                   )  
+             }))
+            
+    }
+
+    return (
+        <div >
+            <div style={{display:'flex',flexWrap:'wrap',width:'65px'}}>
+                 {renderCube(myarry)}
+            </div>
+            <br></br>
+
+        </div>
+    )
+}
+
+export default Rubycubyexamplereact;
